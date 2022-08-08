5fcc43abda4aa954d608a5b2159aa51b2fca5c29
hai nguyen <hainguyen24091996@gmail.com>
2022-08-08 17:43:00 +0700

    lambda expression

---
+++ Reactjs_lession_3_ES6/module/app.js
@@ -0,0 +1,5 @@
+import { greet, Lesson, greeting } from './main.js';
+
+console.log(greet);
+console.log(Lesson);
+console.log("Hải", "Học module trong ES6");
---
+++ Reactjs_lession_3_ES6/module/main.js
@@ -0,0 +1,8 @@
+let greet = "Xin Chào Codegym Online!";
+const Lesson = "Sử dụng module trong ES6";
+
+function greeting(name, content) {
+    return name + " " + content;
+}
+
+export { greet, Lesson, greeting };
