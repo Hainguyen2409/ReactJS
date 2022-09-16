28a903cd7d958e04d5936b71aa68f2b14d07986e
hai nguyen <hainguyen24091996@gmail.com>
2022-09-16 17:30:16 +0700

    add calculator

--- Reactjs_lession_8_StateAndLifeCycle/my-app/demo/src/index.js
+++ Reactjs_lession_8_StateAndLifeCycle/my-app/demo/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import StateInfo from './Demo/StateInfo';
 import Car from './Demo/SetState';
 import Count from './Demo/Count';
@@ -12,10 +13,11 @@ import Color from './Demo/Color';
 import Delete from './Demo/Delete';
 import Login from './Demo/Login';
 import ExpandApp from './Homework/ExpandApp';
+import Calculator from './Homework/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ExpandApp/>
+  <Calculator/>
 );
 
 // If you want to start measuring performance in your app, pass a function
