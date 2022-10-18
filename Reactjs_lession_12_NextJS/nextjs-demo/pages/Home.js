4ce77ab896a4cfeacd8c1f2805f91fec87c4881d
Hai Nguyen <hainguyen24091996@gmail.com>
2022-10-18 23:56:05 +0700

    add demo lession 12

---
+++ Reactjs_lession_12_NextJS/nextjs-demo/pages/Home.js
@@ -0,0 +1,11 @@
+import Layout from "../Components/mainlayout";
+
+export default function Home() {
+    return(
+        <div className="container">
+            <Layout>
+                This is Home.
+            </Layout>
+        </div>
+    )
+}
